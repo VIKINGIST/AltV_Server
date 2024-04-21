e12af9d38e2d6e5f484a4fba54e4cff99642cf9b
VIKINGIST <viktor.kingist@gmail.com>
2024-04-21 15:36:59 +0300

    /tpm v 0.2 - fix texture drops

--- resources/commands/server/index.js
+++ resources/commands/server/index.js
@@ -2,7 +2,7 @@ import * as alt from 'alt-server';
 import * as chat from 'alt:chat';
 
 chat.registerCmd('tpm', (player) => {
-    alt.log(`Команда /tpm викликана гравцем ${player.name}`);
+    //alt.log(`Команда /tpm викликана гравцем ${player.name}`);
     alt.emitClient(player, 'commands:GetMarker');
 });
 
@@ -13,10 +13,10 @@ alt.onClient('commands:SetMarker', (player, markerPos) => {
 
 function handleTeleportToMarker(player, markerPos) {
     if (!player.vehicle) {
-        alt.log(`Координати гравця ${player.name} до телепортації: ${JSON.stringify(player.pos)}`);
-        const { x, y, z } = markerPos;
-        player.pos = new alt.Vector3(x, y, z);
-        alt.log(`Координати гравця ${player.name} після телепортації: ${JSON.stringify(player.pos)}`);
+        //alt.log(`Координати гравця ${player.name} до телепортації: ${JSON.stringify(player.pos)}`);
+        const newPos = { x: markerPos.x, y: markerPos.y, z: markerPos.z + 5.0 }; // Додати 5 метрі до висоти
+        player.pos = new alt.Vector3(newPos.x, newPos.y, newPos.z);
+        //alt.log(`Координати гравця ${player.name} після телепортації: ${JSON.stringify(player.pos)}`);
         alt.emitClient(player, 'commands:Notify', 'Ви телепортувалися до мітки на карті.');
     } else {
         alt.emitClient(player, 'commands:Notify', 'Ви не можете телепортуватися, перебуваючи в транспортному засобі.');
