860e414b593cd11b1b2b9c4dbaeeec98d8508332
VIKINGIST <viktor.kingist@gmail.com>
2024-04-21 13:38:31 +0300

    /tpm 0.1 (work)

--- resources/commands/client/index.js
+++ resources/commands/client/index.js
@@ -1,16 +1,17 @@
-// resources/commands/client/index.js
-import alt from 'alt-client';
+import * as alt from 'alt-client';
+import * as native from 'natives';
 
 alt.onServer('commands:GetMarker', () => {
-  const marker = alt.getWaypointPos();
-  if (marker) {
-    alt.log(`Відправлення координат мітки на сервер: ${JSON.stringify(marker)}`);
-    alt.emitServer('commands:SetMarker', marker);
-  } else {
-    alt.log('Ви не встановили мітку на карті.');
-  }
+    const marker = native.getFirstBlipInfoId(8); // 8 = Waypoint blip
+    if (native.doesBlipExist(marker)) {
+        const markerPos = native.getBlipInfoIdCoord(marker);
+        alt.log(`Відправлення координат мітки на сервер: ${JSON.stringify(markerPos)}`);
+        alt.emitServer('commands:SetMarker', markerPos);
+    } else {
+        alt.log('Ви не встановили мітку на карті.');
+    }
 });
 
 alt.onServer('commands:Notify', (message) => {
-  alt.log(message);
+    alt.log(message);
 });
